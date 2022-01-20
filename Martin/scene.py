5c15cec5d29155cdf5149dbbf4b27b9957e26ca8
martinhughes0722 <60445081+martinhughes0722@users.noreply.github.com>
2022-01-20 14:26:38 -0500

    Buttons move away

--- Martin/scene.py
+++ Martin/scene.py
@@ -4,61 +4,58 @@ import pygame
 import button
 pygame.init()
 
-#--------------Start of program----------------------
-X = 1000
-Y = 600
-#Background color is rbg color number.
+#preset colors
 White = (255,255,255)
 Red = (255,0,0)
 Blue = (202, 228, 241)
 Black = (0,0,0)
-background_color = (Blue)
 
+#Fonts and color for fonts
+font1 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 24)
+font2 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 16)
+
+#--------------Start of program----------------------
+X = 1000
+Y = 600
+background_color = (Blue)
 #fps variable
 clock = pygame.time.Clock()
-
 #Dimensions of the screen for the scene 
 screen = pygame.display.set_mode((X,Y))
-
-#Set the caption of screen
+#Load in images here
+start_img = pygame.image.load('img/start_btn.png').convert_alpha()
+exit_img = pygame.image.load('img/quit_btn.jpg').convert_alpha()
+blank_img = pygame.image.load('img/blank.png').convert_alpha()
+#Set the caption of screen(top left corner words)
 pygame.display.set_caption('2DPhysicsEducationTool')
-
 #set the color of scene 
 screen.fill(background_color)
 
-#information for font on the screen:
-font1 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 24)
-font1Color = (Black)
-text1 = font1.render('2D Physics Education Tool', True, font1Color)
-
-
-
-start_img = pygame.image.load('img/start_btn.png').convert_alpha()
-exit_img = pygame.image.load('img/quit_btn.jpg').convert_alpha()
+#information on screen for our program
+text1 = font1.render('2D Physics Education Tool', True, Black)
 
 #button instances
 start_button = button.Button(100, 200, start_img, 0.45)
 exit_button = button.Button(550, 200, exit_img, 0.35)
 
+#---------------------------------------------------------------
 
 #----------Screen after start button!------------------------
 def physics_selector():
+    
     print('physics selector')
+    text2 = font2.render('Please select which simulation you would like to run :)', True, White)
     X = 1000
-    Y = 600
+    Y = 650
     screen = pygame.display.set_mode((X,Y))
     pygame.display.set_caption("Select Physics Simulation")
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    
-    background.fill((100, 20, 40))
-    screen.blit(background,[0,0])
-    
-    #background_color = (White)
-   # screen.fill(background_color)
-    #screen.blit(screen,[0,0])
-    pygame.display.flip()
 
+    #new background color
+    screen.fill(pygame.Color(100, 20, 140))
+
+    screen.blit(text2,(300,60))
+    pygame.display.flip()
+#--------------------------------------------------------------
 
 
 #-----------where the game runs!-----------------------------
@@ -69,9 +66,13 @@ while running:
     clock.tick(60)
 
     screen.blit(text1,(350,20))
+
     if start_button.draw(screen) == True:
         print("Start")
+        start_button = button.Button(0, 0, blank_img, .000001)
+        exit_button = button.Button(0,0,blank_img, .000001)
         physics_selector()
+
     if exit_button.draw(screen) == True:
         running = False
     #for loop for the event queue
