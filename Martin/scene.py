2fa885964895127a8ae2fa4193f5c0cc990946d6
martinhughes0722 <60445081+martinhughes0722@users.noreply.github.com>
2022-01-20 11:34:08 -0500

    Update scene.py
    Added words on main page!

--- Martin/scene.py
+++ Martin/scene.py
@@ -1,7 +1,11 @@
 #import pygame module 
 from numpy import true_divide
 import pygame
+pygame.init()
 
+
+X = 1000
+Y = 600
 #Background color is rbg color number.
 White = (255,255,255)
 Red = (255,0,0)
@@ -11,7 +15,7 @@ background_color = (White)
 clock = pygame.time.Clock()
 
 #Dimensions of the screen for the scene 
-screen = pygame.display.set_mode((1000,600))
+screen = pygame.display.set_mode((X,Y))
 
 #Set the caption of screen
 pygame.display.set_caption('2DPhysicsEducationTool')
@@ -21,15 +25,29 @@ screen.fill(background_color)
 
 pygame.display.flip()
 
+#information for font on the screen:
+font1 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 24)
+font1Color = (0,150,250)
+text1 = font1.render('2D Physics Education Tool', True, font1Color)
+
+
+
 running = True
 
 #game loop here: 
 
 while running:
     clock.tick(60)
+
+    screen.blit(text1,(350,20))
+
+
     #for loop for the event queue
     for event in pygame.event.get():
 
 
         if event.type == pygame.QUIT:
             running = False
+        
+
+        pygame.display.update()
