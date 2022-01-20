332ab46a2686b7cd9e1a281510f6ad88dc9b8853
martinhughes0722 <60445081+martinhughes0722@users.noreply.github.com>
2022-01-20 13:31:57 -0500

    button stuff
    after clicking start cant get old buttons to go away

--- Martin/scene.py
+++ Martin/scene.py
@@ -1,6 +1,7 @@
 #import pygame module 
 from numpy import true_divide
 import pygame
+import button
 pygame.init()
 
 
@@ -9,7 +10,9 @@ Y = 600
 #Background color is rbg color number.
 White = (255,255,255)
 Red = (255,0,0)
-background_color = (White)
+Blue = (202, 228, 241)
+Black = (0,0,0)
+background_color = (Blue)
 
 #fps variable
 clock = pygame.time.Clock()
@@ -23,47 +26,52 @@ pygame.display.set_caption('2DPhysicsEducationTool')
 #set the color of scene 
 screen.fill(background_color)
 
-
-pygame.draw.rect(screen , Red, pygame.Rect(50,300,60,60),2)
-pygame.draw.rect(screen , Red, pygame.Rect(150,300,60,60))
-pygame.draw.rect(screen , Red, pygame.Rect(250,300,60,60))
-pygame.draw.rect(screen , Red, pygame.Rect(350,300,60,60))
-pygame.draw.rect(screen , Red, pygame.Rect(450,300,60,60))
-
 #information for font on the screen:
 font1 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 24)
-font1Color = (0,150,250)
+font1Color = (Black)
 text1 = font1.render('2D Physics Education Tool', True, font1Color)
+
+
+
+start_img = pygame.image.load('img/start_btn.png').convert_alpha()
+exit_img = pygame.image.load('img/quit_btn.jpg').convert_alpha()
+
+#button instances
+start_button = button.Button(100, 200, start_img, 0.45)
+exit_button = button.Button(550, 200, exit_img, 0.35)
+
+
+
+def physics_selector():
+    print('physics selector')
+    X = 1000
+    Y = 600
+    screen = pygame.display.set_mode((X,Y))
+    pygame.display.set_caption("Select Physics Simulation")
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((20, 20, 40))
+    screen.blit(background,[0,0])
+    #background_color = (White)
+   # screen.fill(background_color)
+    #screen.blit(screen,[0,0])
+    pygame.display.flip()
+
 
 pygame.display.flip()
 
 running = True
-
-start_img = pygame.image.load('img/start_btn.png').convert_alpha()
-exit_img = pygame.image.load('img/quit_btn.png').convert_alpha()
-
-#button class
-class Button():
-    def __init__(self, x, y, image):
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
-
-    def draw (self):
-        #draw on screen
-        screen.blit(self.image, (self.rext.x, self.rect.y))
-#button instances
-start_button = Button(100, 200, start_img)
-exit_button = Button(450, 200, exit_img)
-
 #game loop here: 
 
 while running:
     clock.tick(60)
 
     screen.blit(text1,(350,20))
-
-
+    if start_button.draw(screen) == True:
+        print("Start")
+        physics_selector()
+    if exit_button.draw(screen) == True:
+        running = False
     #for loop for the event queue
     for event in pygame.event.get():
 
