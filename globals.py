4251b1ddcf78a21e1419fa0620013902aaeb4701
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2022-01-23 22:32:49 -0500

    Bouncing ball version 1

---
+++ globals.py
@@ -0,0 +1,12 @@
+import pygame
+
+#Background color is rbg color number.
+White = (255,255,255)
+Red = (255,0,0)
+background_color = (White)
+
+#Dimensions of the screen for the scene 
+screen_width = 1000
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width,screen_height))
