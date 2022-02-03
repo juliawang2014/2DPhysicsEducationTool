c03487fa62ce6786761c9dfce2e97b45bebdb28e
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2022-02-03 18:15:54 -0500

    Made create_ball independent

--- Julia/globals.py
+++ Julia/globals.py
@@ -1,9 +1,15 @@
 import pygame
+import pymunk
 
 #Background color is rbg color number.
 White = (255,255,255)
 Red = (255,0,0)
 background_color = (White)
+
+space = pymunk.Space()
+gravity = (0.0, 900.0)
+
+clock = pygame.time.Clock()
 
 #Dimensions of the screen for the scene 
 screen_width = 1000
