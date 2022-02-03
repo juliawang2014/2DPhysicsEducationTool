261c0cae2787666fa1cdbfc035b28a27b0c33de0
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2022-02-03 10:25:42 -0500

    Testing of Textbox
    Textbox testing

---
+++ Reed/Start.py
@@ -0,0 +1,35 @@
+import pygame
+import pygame_gui
+from pygame_gui.elements import UIHorizontalSlider
+
+from pygame_gui import UIManager
+
+
+pygame.init()
+
+pygame.display.set_caption('Physics Tutorial')
+window = pygame.display.set_mode((1000,600))
+clock = pygame.time.Clock()
+
+backcolor = pygame.Surface((1000,600))
+backcolor.fill(pygame.color('#FFFF00'))
+
+guimanager= pygame_gui.UIManager((1000,600))
+
+circslide = UIHorizontalSlider(pygame.Rect((500,100),0,(0,100)),manager=guimanager)
+
+while True:
+    time_delta = clock.tick(60)/1000.0
+    #used to close program
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        
+        guimanager.process_events(event)
+
+    guimanager.update(time_delta)
+
+    window.blit(backcolor, (0,0))
+    guimanager.draw_ui(window)
+
+    pygame.display.update()
