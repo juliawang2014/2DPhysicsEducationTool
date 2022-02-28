e8c90e94fa65f09bb1dd2cc428fb82ad0b9fbb05
martinhughes0722 <60445081+martinhughes0722@users.noreply.github.com>
2022-02-28 14:41:35 -0500

    Update Plinko.py
    reset kinda is working clicking r resets clicking d brings back barriers

--- Martin/Plinko.py
+++ Martin/Plinko.py
@@ -23,7 +23,7 @@ def _add_static_scenery(self) -> None:
 def create_coin(space,pos):
     body = pymunk.Body(1,100,body_type = pymunk.Body.DYNAMIC)
     body.position = pos
-    shape = pymunk.Circle(body,30)
+    shape = pymunk.Circle(body,27)
     space.add(body,shape)
     return shape
 
@@ -31,14 +31,14 @@ def draw_coins(coins):
     for coin in coins:
         pos_x = int(coin.body.position.x)
         pos_y = int(coin.body.position.y)
-        pygame.draw.circle(screen,(0,0,0),(pos_x,pos_y),30)
+        pygame.draw.circle(screen,(0,0,0),(pos_x,pos_y),27)
         coin_rect = coin_surface.get_rect(center = (pos_x,pos_y))
         screen.blit(coin_surface,coin_rect)
 
 def static_ball(space,pos):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
     body.position = pos
-    shape = pymunk.Circle(body,15)
+    shape = pymunk.Circle(body,18)
     space.add(body,shape)
     return shape
 
@@ -46,7 +46,7 @@ def draw_static_ball(balls):
     for ball in balls:
         pos_x = int(ball.body.position.x)
         pos_y = int(ball.body.position.y)
-        pygame.draw.circle(screen,(255,255,255),(pos_x,pos_y),15)
+        pygame.draw.circle(screen,(255,255,255),(pos_x,pos_y),18)
 
 def static_wall(space,pos):
     
@@ -84,7 +84,7 @@ def reset():
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000,600))
+screen = pygame.display.set_mode((1000,750))
 clock = pygame.time.Clock()
 space = pymunk.Space()
 space.gravity = (0,150)
@@ -96,82 +96,72 @@ coin_surface = pygame.image.load('img/coin.png')
 balls = []
 #plinko setup
 #first row
-balls.append(static_ball(space,(100,100)))
-balls.append(static_ball(space,(200,100)))
-balls.append(static_ball(space,(300,100)))
-balls.append(static_ball(space,(400,100)))
-balls.append(static_ball(space,(500,100)))
-balls.append(static_ball(space,(600,100)))
-balls.append(static_ball(space,(700,100)))
-balls.append(static_ball(space,(800,100)))
-balls.append(static_ball(space,(900,100)))
-
+balls.append(static_ball(space,(100,140)))
+balls.append(static_ball(space,(300,140)))
+balls.append(static_ball(space,(400,140)))
+balls.append(static_ball(space,(500,140)))
+balls.append(static_ball(space,(600,140)))
+balls.append(static_ball(space,(700,140)))
+balls.append(static_ball(space,(900,140)))
 #second row
-balls.append(static_ball(space,(30,175)))
-balls.append(static_ball(space,(150,175)))
-balls.append(static_ball(space,(250,175)))
-balls.append(static_ball(space,(350,175)))
-balls.append(static_ball(space,(450,175)))
-balls.append(static_ball(space,(550,175)))
-balls.append(static_ball(space,(650,175)))
-balls.append(static_ball(space,(750,175)))
-balls.append(static_ball(space,(850,175)))
-balls.append(static_ball(space,(970,175)))
-
+balls.append(static_ball(space,(0,215)))
+balls.append(static_ball(space,(200,215)))
+balls.append(static_ball(space,(350,215)))
+balls.append(static_ball(space,(450,215)))
+balls.append(static_ball(space,(550,215)))
+balls.append(static_ball(space,(650,215)))
+balls.append(static_ball(space,(800,215)))
+balls.append(static_ball(space,(1000,215)))
 #3rd row
-balls.append(static_ball(space,(100,250)))
-balls.append(static_ball(space,(200,250)))
-balls.append(static_ball(space,(300,250)))
-balls.append(static_ball(space,(400,250)))
-balls.append(static_ball(space,(500,250)))
-balls.append(static_ball(space,(600,250)))
-balls.append(static_ball(space,(700,250)))
-balls.append(static_ball(space,(800,250)))
-balls.append(static_ball(space,(900,250)))
-
+balls.append(static_ball(space,(100,290)))
+balls.append(static_ball(space,(300,290)))
+balls.append(static_ball(space,(400,290)))
+balls.append(static_ball(space,(500,290)))
+balls.append(static_ball(space,(600,290)))
+balls.append(static_ball(space,(700,290)))
+balls.append(static_ball(space,(900,290)))
 #4th row
-balls.append(static_ball(space,(30,325)))
-balls.append(static_ball(space,(150,325)))
-balls.append(static_ball(space,(250,325)))
-balls.append(static_ball(space,(350,325)))
-balls.append(static_ball(space,(450,325)))
-balls.append(static_ball(space,(550,325)))
-balls.append(static_ball(space,(650,325)))
-balls.append(static_ball(space,(750,325)))
-balls.append(static_ball(space,(850,325)))
-balls.append(static_ball(space,(970,325)))
+balls.append(static_ball(space,(0,365)))
+balls.append(static_ball(space,(150,365)))
+balls.append(static_ball(space,(250,365)))
+balls.append(static_ball(space,(350,365)))
+balls.append(static_ball(space,(450,365)))
+balls.append(static_ball(space,(550,365)))
+balls.append(static_ball(space,(650,365)))
+balls.append(static_ball(space,(750,365)))
+balls.append(static_ball(space,(850,365)))
+balls.append(static_ball(space,(1000,365)))
 #5th row
-balls.append(static_ball(space,(100,400)))
-balls.append(static_ball(space,(200,400)))
-balls.append(static_ball(space,(300,400)))
-balls.append(static_ball(space,(400,400)))
-balls.append(static_ball(space,(500,400)))
-balls.append(static_ball(space,(600,400)))
-balls.append(static_ball(space,(700,400)))
-balls.append(static_ball(space,(800,400)))
-balls.append(static_ball(space,(900,400)))
-
+balls.append(static_ball(space,(75,440)))
+balls.append(static_ball(space,(200,440)))
+balls.append(static_ball(space,(300,440)))
+balls.append(static_ball(space,(400,440)))
+balls.append(static_ball(space,(500,440)))
+balls.append(static_ball(space,(600,440)))
+balls.append(static_ball(space,(700,440)))
+balls.append(static_ball(space,(800,440)))
+balls.append(static_ball(space,(925,440)))
 #6th row
-balls.append(static_ball(space,(30,475)))
-balls.append(static_ball(space,(150,475)))
-balls.append(static_ball(space,(250,475)))
-balls.append(static_ball(space,(350,475)))
-balls.append(static_ball(space,(450,475)))
-balls.append(static_ball(space,(550,475)))
-balls.append(static_ball(space,(650,475)))
-balls.append(static_ball(space,(750,475)))
-balls.append(static_ball(space,(850,475)))
-balls.append(static_ball(space,(970,475)))
+balls.append(static_ball(space,(0,515)))
+balls.append(static_ball(space,(150,515)))
+balls.append(static_ball(space,(250,515)))
+balls.append(static_ball(space,(350,515)))
+balls.append(static_ball(space,(450,515)))
+balls.append(static_ball(space,(550,515)))
+balls.append(static_ball(space,(650,515)))
+balls.append(static_ball(space,(750,515)))
+balls.append(static_ball(space,(850,515)))
+balls.append(static_ball(space,(1000,515)))
 #7th row
-balls.append(static_ball(space,(100,550)))
-balls.append(static_ball(space,(200,550)))
-balls.append(static_ball(space,(300,550)))
-balls.append(static_ball(space,(400,550)))
-balls.append(static_ball(space,(500,550)))
-balls.append(static_ball(space,(600,550)))
-balls.append(static_ball(space,(700,550)))
-balls.append(static_ball(space,(800,550)))
-balls.append(static_ball(space,(900,550)))
+balls.append(static_ball(space,(100,590)))
+balls.append(static_ball(space,(200,590)))
+balls.append(static_ball(space,(300,590)))
+balls.append(static_ball(space,(400,590)))
+balls.append(static_ball(space,(500,590)))
+balls.append(static_ball(space,(600,590)))
+balls.append(static_ball(space,(700,590)))
+balls.append(static_ball(space,(800,590)))
+balls.append(static_ball(space,(900,590)))
 
 
 draw_static_wall()
@@ -180,6 +170,15 @@ pause = True
 
 physics_steps_per_frame = 1
 dt = 1.0 / 60.0
+
+
+def draw_boundary():
+    
+    draw_static_ball(balls)
+
+    draw_static_wall()
+    pygame.display.update()
+
 
 while running:
     for event in pygame.event.get():
@@ -195,6 +194,10 @@ while running:
                     space.step(dt)
         if reset_b:
             reset()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+            reset_b = False
+            draw_boundary()
+            draw_static_ball(balls)
     screen.fill((0,150,255))
     draw_coins(coins)
     draw_static_ball(balls)
