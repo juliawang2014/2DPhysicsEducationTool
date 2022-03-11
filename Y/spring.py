e7dfa74585cfcd3d3f5fb0f32c6e5246bd133464
Y Duong <55992627+duongy18418@users.noreply.github.com>
2022-03-11 14:35:53 -0500

    Update Spring.py
    Create spring mass system

--- Y/spring.py
+++ Y/spring.py
@@ -1,44 +1,56 @@
-from turtle import circle
-import pygame
-import pymunk
-import pymunk.pygame_util
+from vpython import *
 
-import sys
-sys.path.append('Julia')
-import globals
+canvas(height=100, length=1000)
 
-space = globals.space
+def acc():
+    dr_right = ball.pos - wall_right.pos
+    force_right = -spring_right.constant*(mag(dr_right) - length)*norm(dr_right)
+    
+    dr_left = ball.pos - wall_left.pos
+    force_left = -spring_left.constant*(mag(dr_left) - length)*norm(dr_left)
+    
+    return (force_right+force_left) / ball.mass
 
-class create_spring:
-    b0 = space.static_body
-    p0 = 100, 200
+def change_radius(widget):
+    print('radius:', widget.value)
+    ball.radius = widget.value 
 
-    body = pymunk.Body(mass=1, moment=10)
-    body.position = (100, 50)
-    circle = pymunk.Circle(body, radius=30)
+def change_massball(widget):
+    print('massball:', widget.value)
+    ball.mass = widget.value 
 
-    joint = pymunk.constraints.DampedSpring(b0, body, p0, (20, 0), 100, 50, 1)
-    space.add(body, joint)
+# --- main ---
 
-class App:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((700, 240))
-        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
-        self.running = True
+length = 30.
+fric = 0.01
+massball = 1
+mass = massball
+radius = 2.5
 
-    def run(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    pygame.image.save(self.screen, 'shape.png')
+scene2 = canvas(title='Mass Spring System', width=400, height=200, center=vector(0,0,0), background=color.white) 
 
-            self.screen.fill((220, 220, 220))
-            space.debug_draw(self.draw_options)
-            pygame.display.update()
-            space.step(0.01)
+ball = sphere(pos=vector(2,0,0), velocity=vector(0,0,0), radius=radius, mass=massball, color=color.blue)
 
-        pygame.quit()
+wall_right = box(pos=vector(length,0,0), size=vector(0.2, 5, 5), color=color.green)
+wall_left = box(pos=vector(-length,0,0), size=vector(0.2, 5, 5), color=color.green)
 
-App().run()
+spring_right = helix(pos=wall_right.pos, axis=ball.pos-wall_right.pos, constant=1, coils=10, thickness=0.2, radius=1, color=color.red)
+spring_left = helix(pos=wall_left.pos, axis=ball.pos-wall_left.pos, constant=1, coils=10, thickness=0.2, radius=1, color=color.red)
+
+sl1 = slider(min=0.3, max=10, value=massball, length=220, bind=change_massball, right=15)
+sl2 = slider(min=0.3, max=10, value=radius,   length=220, bind=change_radius,   right=15)
+
+
+ball.trail = curve(color=ball.color)
+
+t = 0
+dt = 0.01
+
+while t < 100:
+    rate(500)
+    ball.velocity = ball.velocity + acc() * dt
+    ball.pos = ball.pos + ball.velocity * dt
+    spring_right.axis = ball.pos - wall_right.pos
+    spring_left.axis = ball.pos - wall_left.pos
+    ball.trail.append(pos=ball.pos)
+    t = t + dt
