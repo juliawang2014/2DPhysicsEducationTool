dc3474c21e52b1badfdf8c08da745bb7d7c635bd
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2022-03-17 00:45:18 -0400

    add timer for when the ball reaches the end

--- Brendan/gravity.py
+++ Brendan/gravity.py
@@ -61,12 +61,18 @@ class BouncyBalls(object):
         #self.ui_slider1 = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(relative_rect=pygame.Rect((10, 470), (250, 50)), start_value=25, value_range=(1, 100), manager=self.manager, object_id="size") #ball size
         self.ui_slider2 = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(relative_rect=pygame.Rect((10, 540), (250, 50)), start_value=900, value_range=(0, 2000), manager=self.manager, click_increment=100, object_id="gravity") #gravity
         self.ui_textbox = pygame_gui.elements.ui_text_box.UITextBox(html_text="Gravity: "+str(self._space.gravity.int_tuple[1]), relative_rect=pygame.Rect((300, 540), (250, 50)), manager=self.manager, object_id="gravityInfoTextBox")
+        self.ui_textbox2 = pygame_gui.elements.ui_text_box.UITextBox(html_text="", relative_rect=pygame.Rect((600, 340), (350, 250)), manager=self.manager, object_id="doneBox")
+        self.done_box_text = ""
         self.spawn_button = pygame_gui.elements.ui_button.UIButton(relative_rect=pygame.Rect((10, 270), (250, 50)), text="Spawn Ball", manager=self.manager, object_id="spawn")
         #self.ui_textbox.set_active_effect(pygame_gui.TEXT_EFFECT_TYPING_APPEAR,params={'time_per_letter':1})
         self.time_delta = 0.0
         
         #BALL SIZE
         self.ball_size = 25 #default is 25 from shapes.py
+        
+        self.ball_start_time = 0 #time object used to see when the ball is spawned
+        self.ball_end_time = 0
+        self.is_ball_done_rolling = False #turns true once ball is at the bottom of the ramp
 
     def run(self) -> None:
         """
@@ -79,6 +85,7 @@ class BouncyBalls(object):
             for _ in range(self._physics_steps_per_frame):
                 self._space.step(self._dt)
 
+            self._handle_ball_timing()
             self._process_events()
             self._clear_screen()
             self._draw_objects()
@@ -103,7 +110,7 @@ class BouncyBalls(object):
         ]
         for line in static_lines:
             line.elasticity = 0.0
-            line.friction = 10.0
+            line.friction = 0.0
         self._space.add(*static_lines)
 
     def _process_events(self) -> None:
@@ -123,13 +130,15 @@ class BouncyBalls(object):
                 if event.ui_object_id == "spawn":
                     self._deleteAllBalls()
                     shapes.create_ball(self, (33,173), 10, self.ball_size, elasticity=0.1)
+                    self.is_ball_done_rolling = False
+                    self.ball_start_time = datetime.datetime.now()
             elif event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
                 if event.ui_object_id == "gravity":
                     if event.value != self._space.gravity.int_tuple[1]:
                         self._space.gravity = (0, event.value)
                         self.ui_textbox.clear_text_surface()
                         self.ui_textbox.set_text("Gravity: " + str(self._space.gravity.int_tuple[1]))         
-                
+
             self.manager.process_events(event)
 
     def _clear_screen(self) -> None:
@@ -154,6 +163,21 @@ class BouncyBalls(object):
         for ball in self._balls:
             self._space.remove(ball)
         self._balls = []
+        
+    def _handle_ball_timing(self) -> None:
+        """take care of some timing stuff"""
+        #if self._balls:
+                #print(self._balls[0].body.position.x)
+            #self.ui_textbox2.set_text(str(self._balls[0].body.position.x))
+            #self.ui_textbox2.rebuild()
+        if self._balls and self._balls[0].body.position.x > 974 and not self.is_ball_done_rolling:
+            #section to calculate time delta
+            self.ball_end_time = datetime.datetime.now()
+            time_delta = self.ball_end_time - self.ball_start_time
+            
+            self.done_box_text += "ball done rolling, time:" + str(time_delta) + "<br>"
+            self.ui_textbox2.set_text(self.done_box_text)
+            self.is_ball_done_rolling = True
 
 
 if __name__ == "__main__":
