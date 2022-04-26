5e1be6867d46d994dcac30f3fc0fb74f02f0ae9a
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2022-04-25 21:36:10 -0400

    Revert "test out filenametranslator"
    This reverts commit f76d3f813946e60e757ea51c3eb91cb0d887d278.

--- Application/fileNameTranslator.py
+++
@@ -1,7 +0,0 @@
-"""main ideas taken from https://stackoverflow.com/questions/9553262/pyinstaller-ioerror-errno-2-no-such-file-or-directory"""
-import os
-
-def translateFileName(fileName):
-    if '_MEIPASS2' in os.environ:
-        fileName = os.path.join(os.environ['_MEIPASS2'], fileName))
-    return fileName
